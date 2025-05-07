88f095db74a2c6bba3e16015e63a971a85898895
alux79 <ung.alexander79@gmail.com>
2025-05-07 15:05:04 -0400

    Add files via upload

---
+++ software/finalProjectCentral/target/6.2.1/p2/obj/src/user_module.o.d
@@ -0,0 +1,7 @@
+c\:/Users/ungal/EE1301/RemoteDoorLock-main/software/finalProjectCentral/target/6.2.1/p2//obj/./src/user_module.o: \
+ src/user_module.c \
+ ../../../modules/shared/rtl872x/inc/user-part/user_module.inc \
+ ../../../dynalib/inc/dynalib.h ../../../hal/shared/platforms.h
+../../../modules/shared/rtl872x/inc/user-part/user_module.inc:
+../../../dynalib/inc/dynalib.h:
+../../../hal/shared/platforms.h:
